ff3949cd473c6705773d2705374feda27a8e463a
ye zhang <aresyeah@gmail.com>
2019-09-16 20:29:25 -0700

    Updates

---
+++ jscalculator/precache-manifest.e87eabd3a6ea7f0800583e5519202abd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe289d4db686df9b69d7b2d0cd1bae4",
+    "url": "/jscalculator/index.html"
+  },
+  {
+    "revision": "7685a7413368a213e736",
+    "url": "/jscalculator/static/css/main.ac1ec2e6.chunk.css"
+  },
+  {
+    "revision": "ab51033a893192aa5bfb",
+    "url": "/jscalculator/static/js/2.f809bb8c.chunk.js"
+  },
+  {
+    "revision": "7685a7413368a213e736",
+    "url": "/jscalculator/static/js/main.fdd81273.chunk.js"
+  },
+  {
+    "revision": "83ca8e86e4a9f7d57e77",
+    "url": "/jscalculator/static/js/runtime~main.114b30cb.js"
+  }
+]);
