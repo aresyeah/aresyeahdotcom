a4e6dc43ca09b23656fa289c2a190f746c9a1278
ye zhang <aresyeah@gmail.com>
2019-09-17 22:51:06 -0700

    use quote machine

---
+++ quotemachine/precache-manifest.66008959e61be68f9ab789ae56dc48b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fc43595f98d037cc55a2ec7be6b3e4",
+    "url": "/quotemachine/index.html"
+  },
+  {
+    "revision": "599bc385fb0dc8a6b52f",
+    "url": "/quotemachine/static/css/main.8b796ca9.chunk.css"
+  },
+  {
+    "revision": "d0180b39e92ccbd759b8",
+    "url": "/quotemachine/static/js/2.8ee652d0.chunk.js"
+  },
+  {
+    "revision": "599bc385fb0dc8a6b52f",
+    "url": "/quotemachine/static/js/main.4d18f3a4.chunk.js"
+  },
+  {
+    "revision": "84414e39dcc9f9ea499f",
+    "url": "/quotemachine/static/js/runtime~main.0c7af691.js"
+  }
+]);
