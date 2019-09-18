58747ef77fa041e953bf34189a02c1d272736437
ye zhang <aresyeah@gmail.com>
2019-09-17 21:32:38 -0700

    serve from master branch

---
+++ markdown/precache-manifest.74d85a94e664f617bc8c8f4133316c83.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86abc17732ec47a925f8d6df74cb9f37",
+    "url": "/markdown/index.html"
+  },
+  {
+    "revision": "4d4a0faab10b84790ba4",
+    "url": "/markdown/static/js/2.2c43c2be.chunk.js"
+  },
+  {
+    "revision": "ec754d7937faef3aa732",
+    "url": "/markdown/static/js/main.aaeb3840.chunk.js"
+  },
+  {
+    "revision": "3b6b19a9967789047617",
+    "url": "/markdown/static/js/runtime~main.22de758b.js"
+  }
+]);
---
+++ pomodoroclock/precache-manifest.28d1daefb5008b8af42afd0bdfb01b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cbef43a27cc1aae3b57a2de8041407",
+    "url": "/pomodoroclock/index.html"
+  },
+  {
+    "revision": "05ee618a275847efea6b",
+    "url": "/pomodoroclock/static/css/main.ae3d87d9.chunk.css"
+  },
+  {
+    "revision": "fe68299f4e19c00fdc89",
+    "url": "/pomodoroclock/static/js/2.018a590c.chunk.js"
+  },
+  {
+    "revision": "05ee618a275847efea6b",
+    "url": "/pomodoroclock/static/js/main.4cccf0c1.chunk.js"
+  },
+  {
+    "revision": "4471eec29305687892ca",
+    "url": "/pomodoroclock/static/js/runtime~main.7d676cbd.js"
+  }
+]);
